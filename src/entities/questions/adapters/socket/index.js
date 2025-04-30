f152c6229cc2008d42260dd414edb89d5fc1f0a7
Franck Cai <senlouboi.egame@gmail.com>
2025-04-30 16:02:36 +0200

    question socket update

--- src/entities/questions/adapters/socket/index.js
+++ src/entities/questions/adapters/socket/index.js
@@ -2,14 +2,6 @@ import { socketHandler } from "@Application/middlewares/error-handler";
 import Controller from "../../controller";
 
 const StartSocketServer = (io, socket) => {
-  console.log("Question socket active");
-  socket.on(
-    "creating-questions",
-    socketHandler(async (roomId) => {
-      Controller.get({roomId})
-      io.emit("alguienTermino", roomId);
-    })
-  );
 
 };
 
